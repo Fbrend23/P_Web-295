ce26f0fa9ce02548621c810907f11e259e1c5f59
Brendan Fleurdelys <fbrend23@gmail.com>
2025-10-03 14:39:28 +0200

    wip do not merge

--- passion-lecture/app/controllers/comments_controller.ts
+++ passion-lecture/app/controllers/comments_controller.ts
@@ -1,4 +1,35 @@
-// import type { HttpContext } from '@adonisjs/core/http'
+import type { HttpContext } from '@adonisjs/core/http'
+import Comment from '#models/comment'
 
 export default class CommentsController {
+  //Afficher tous les commentaires de book_id
+  // /books/#id/comments
+  async index({ params, response }: HttpContext) {
+    //Récupération des commentaire avec le book_id
+    const comments = await Comment.findOrFail(params.book_id)
+    //Chargement des commentaire et des users
+    // await comments.load('comment', (query) => {
+    //   query.preload('user')
+    // })
+    response.ok(comments)
+    return comments
+  }
+
+  async store({ params, request, response }: HttpContext) {
+    //TODO validator
+
+    const { comment } = request.only(['comment'])
+    // TODO auth
+    // const user = auth.user!
+    // const userId = user.id
+
+    // Création du commentaire
+
+    const commentaire = await Comment.create({
+      comment,
+      bookId: params.book_id,
+      //userId
+    })
+    return response.created(commentaire)
+  }
 }
--- passion-lecture/app/controllers/evaluations_controller.ts
+++ passion-lecture/app/controllers/evaluations_controller.ts
@@ -1,4 +1,31 @@
-// import type { HttpContext } from '@adonisjs/core/http'
+import Evaluation from '#models/evaluation'
+import type { HttpContext } from '@adonisjs/core/http'
 
-export default class EvaluationsController {
-}
+// export default class EvaluationsController {
+//   //Afficher toutes les évaluations de book_id
+//   async index({ response }: HttpContext) {
+//     const evaluation = await Evaluation.query().preload('book').cou
+//     response.ok(evaluation)
+//     return evaluation
+//   }
+
+//   async store({ params, request, response }: HttpContext) {
+//     //TODO validator
+
+//     const { comment } = request.only(['comment'])
+//     // TODO auth
+//     // const user = auth.user!
+//     // const userId = user.id
+
+//     // Création du commentaire
+
+//     const commentaire = await Comment.create({
+//       comment,
+//       bookId: params.book_id,
+//       //userId
+//     })
+//     return response.created(commentaire)
+//   }
+// }
+
+// }
