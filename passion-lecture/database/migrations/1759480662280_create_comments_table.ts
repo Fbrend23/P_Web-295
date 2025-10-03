ada1cdcb51d2c5efdc78a77dd83dde9f44a98833
mina-etml <pk60ftv@eduvaud.ch>
2025-10-03 10:42:03 +0200

    feat(comment): model & migration

---
+++ passion-lecture/database/migrations/1759480662280_create_comments_table.ts
@@ -0,0 +1,21 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'comments'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id_book')
+      table.increments('id_user')
+
+      table.string('comment').notNullable()
+
+      table.timestamp('created_at')
+      table.timestamp('updated_at')
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
