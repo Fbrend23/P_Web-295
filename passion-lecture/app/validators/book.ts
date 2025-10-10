204cd7b0a4d67c46bea3a45ebff5ccfa11130b16
Maël <m01@naudet.ch>
2025-10-10 14:44:20 +0200

    fix(validator books): verification id author et categories pour que ce soit dans la db dans le post

--- passion-lecture/app/validators/book.ts
+++ passion-lecture/app/validators/book.ts
@@ -1,13 +1,17 @@
 import Author from '#models/author'
+import Category from '#models/category'
 import vine from '@vinejs/vine'
 
 const author = await Author.all()
 const authorIds = await author.map((s) => s.id)
 
+const category = await Category.all()
+const categoryIds = category.map((c) => c.id)
+
 const bookValidator = vine.compile(
   vine.object({
     title: vine.string().minLength(2).maxLength(255),
-    categoryId: vine.number(),
+    categoryId: vine.number().in(categoryIds),
     authorId: vine.number().in(authorIds),
     numberOfPages: vine.number(),
     pdfLink: vine.string().minLength(2).maxLength(255),
