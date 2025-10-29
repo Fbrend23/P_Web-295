ae7a084723b675a952f6ab1c7a836d58ffa916f6
Brendan Fleurdelys <fbrend23@gmail.com>
2025-10-29 14:42:14 +0100

    feat(validator) Ajout de la validation pour category

--- passion-lecture/app/validators/category.ts
+++ passion-lecture/app/validators/category.ts
@@ -1,7 +1,14 @@
 import vine from '@vinejs/vine'
 const categoryValidator = vine.compile(
   vine.object({
-    label: vine.string().minLength(2).maxLength(255),
+    label: vine
+      .string()
+      .minLength(2)
+      .maxLength(255)
+      .unique(async (query, field) => {
+        const category = await query.from('categories').where('label', field).first()
+        return !category
+      }),
   })
 )
 export { categoryValidator }
