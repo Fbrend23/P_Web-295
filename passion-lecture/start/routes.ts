ad2d12a1f2d3de699e7e08b92c857cf794097c05
Brendan Fleurdelys <158816892+Fbrend23@users.noreply.github.com>
2026-01-26 10:43:05 +0100

    feat: byebye authentication

--- passion-lecture/start/routes.ts
+++ passion-lecture/start/routes.ts
@@ -10,7 +10,6 @@
 
 import AuthController from '#controllers/auth_controller'
 import router from '@adonisjs/core/services/router'
-import { middleware } from './kernel.js'
 import CommentsController from '#controllers/comments_controller'
 import BooksController from '#controllers/books_controller'
 import CategoriesController from '#controllers/categories_controller'
@@ -34,61 +33,60 @@ router.get('/categories/:category_id/books', [CategoryBooksController, 'index'])
 router.get('/uploads/books/images/:fileName', [FilesController, 'getImage'])
 router.get('/uploads/books/pdf/:fileName', [FilesController, 'getPDF'])
 
-router
-  .group(() => {
-    // CRUD Books
-    router
-      .group(() => {
-        router.get(':book_id', [BooksController, 'show'])
-        router.post('', [BooksController, 'store'])
-        router.put(':book_id', [BooksController, 'update'])
-        router.delete(':book_id', [BooksController, 'destroy'])
-      })
-      .prefix('books')
-    // CRUD comments
-    router
-      .group(() => {
-        router.resource('comments', CommentsController).apiOnly()
-      })
-      .prefix('books/:book_id')
-    // CRUD evaluations
-    router
-      .group(() => {
-        router.get('evaluations', [EvaluationsController, 'index'])
-        router.post('evaluations', [EvaluationsController, 'store'])
-        router.get('evaluations/me', [EvaluationsController, 'showMe'])
-      })
-      .prefix('books/:book_id')
+router.group(() => {
+  // CRUD Books
+  router
+    .group(() => {
+      router.get(':book_id', [BooksController, 'show'])
+      router.post('', [BooksController, 'store'])
+      router.put(':book_id', [BooksController, 'update'])
+      router.delete(':book_id', [BooksController, 'destroy'])
+    })
+    .prefix('books')
+  // CRUD comments
+  router
+    .group(() => {
+      router.resource('comments', CommentsController).apiOnly()
+    })
+    .prefix('books/:book_id')
+  // CRUD evaluations
+  router
+    .group(() => {
+      router.get('evaluations', [EvaluationsController, 'index'])
+      router.post('evaluations', [EvaluationsController, 'store'])
+      router.get('evaluations/me', [EvaluationsController, 'showMe'])
+    })
+    .prefix('books/:book_id')
 
-    // CRUD authors
-    router.resource('authors', AuthorsController).apiOnly()
+  // CRUD authors
+  router.resource('authors', AuthorsController).apiOnly()
 
-    // CRUD categories
-    router
-      .group(() => {
-        router.post('', [CategoriesController, 'store'])
-        // router.put(':category_id', [CategoriesController, 'update'])
-        // router.delete(':category_id', [CategoriesController, 'destroy'])
-      })
-      .prefix('categories')
+  // CRUD categories
+  router
+    .group(() => {
+      router.post('', [CategoriesController, 'store'])
+      // router.put(':category_id', [CategoriesController, 'update'])
+      // router.delete(':category_id', [CategoriesController, 'destroy'])
+    })
+    .prefix('categories')
 
-    // CRUD users
-    router.resource('users', UsersController).apiOnly()
+  // CRUD users
+  router.resource('users', UsersController).apiOnly()
 
-    // Show own books
-    router
-      .group(() => {
-        router.resource('books', UserBooksController).apiOnly()
-      })
-      .prefix('users/:user_id')
-  })
-  .use(middleware.auth())
+  // Show own books
+  router
+    .group(() => {
+      router.resource('books', UserBooksController).apiOnly()
+    })
+    .prefix('users/:user_id')
+})
 
 // Authentication paths
 router
   .group(() => {
     router.post('register', [AuthController, 'register'])
     router.post('login', [AuthController, 'login'])
-    router.post('logout', [AuthController, 'logout']).use(middleware.auth())
+    router.post('logout', [AuthController, 'logout'])
+    router.post('logout', [AuthController, 'logout'])
   })
   .prefix('user')
